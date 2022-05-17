9ae24247cf5bcbe9993919f64f9d2e281a253340
Garnett <Joyce.Garnett@magna.com>
2022-05-17 17:18:52 -0400

    fixed core gameplay; added skip option

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,41 +1,78 @@
-// player variables
-var playerName = window.prompt("What is your robot's name?");
-var playerHP = 100;
-var playerAtk = 10;
+    // player variables
+    var playerName = "Test";//window.prompt("What is your robot's name?");
+    var playerHP = 100;
+    var playerAtk = 10;
+    var playerMoney = 10;
 
 
-// enemy variables
-var enemyName = "Roborto";
-var enemyHP = 50;
-var enemyAtk = 12;
+    // enemy variables
+    var enemyName = "Roborto";
+    var enemyHP = 50;
+    var enemyAtk = 12;
 
 var fight = function() {
+    // attack function
+    var attack = function(attackerName, attackerAtk, opponentName, opponentHP) {
+        opponentHP -= attackerAtk;
+        console.log(attackerName + " attacked " + opponentName + ". " + opponentName + " now has " + opponentHP + ".");
+
+        // checks HP
+        if (opponentHP <= 0) {
+            window.alert(opponentName + " has died");
+        }
+        else {
+            window.alert(opponentName + " still has " + opponentHP + " HP.");
+        }
+    }
+
     // round start
     window.alert("Welcome to Robot Gladiators");
 
-    // player attacks enemy
-    enemyHP -= playerAtk;
-    console.log("Player attacks enemy.");
+     // check if player wants to fight
+    var promptFight = window.prompt("FIGHT or SKIP this battle. Enter 'FIGHT' or 'SKIP' to choose.")
 
-    // enemy attacks player
-    playerHP -= enemyAtk;
-    console.log("Enemy attacks player.");
+    
+    if (promptFight === "fight" || promptFight === "FIGHT") {
+        // player attacks enemy
+        enemyHP -= playerAtk;
+        console.log(playerName+" attacked "+enemyName+". "+enemyName+" now has "+enemyHP+" HP.");
+        
+        // checks enemy HP
+        if (enemyHP <= 0) {
+            window.alert(enemyName + " has died!");
+        }
+        else {
+            window.alert(enemyName + " still has " + enemyHP + " HP.")
+        }
 
-    // fight log
-    console.log("Player HP: " + playerHP);
-    console.log("Enemy HP: " + playerHP);
+        // enemy attacks player
+        playerHP -= enemyAtk;
+        console.log(enemyName+" attacked "+playerName+". "+playerName+" now has "+playerHP+" HP.");
 
-    checkHP(playerName, playerHP);
-    checkHP(enemyName, enemyHP);
-}
+        // checks player hp
+        if (playerHP <= 0) {
+            window.alert(playerName + " has died!");
+        }
+        else {
+            window.alert(playerName + " still has " + playerHP + " HP.");
+        }
+    }
+    else if (promptFight === "skip" || promptFight === "SKIP") {
+        console.log(playerName + " has chosen to skip the fight!");
 
-// check HP
-var checkHP = function(Name, HP) {
-    if (HP <= 0) {
-        window.alert(Name + " has died");
+        // skip confirmation
+        var confirmSkip = window.prompt("Are you sure you'd like to quit? Type 'Y' or 'N'");
+        if (confirmSkip === "Y" || confirmSkip === "y"){
+            window.alert(playerName + " has skipped the fight. Goodybye!");
+            playerMoney -= 2;
+        }
+        else {
+            fight();
+        }
     }
     else {
-        window.alert(Name + " still has " + HP + " HP.");
+        window.alert("Choose a valid option. Try again.");
+        fight();
     }
 }
 
